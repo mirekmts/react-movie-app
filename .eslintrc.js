dcbdbc75ca9e7685933056f2486c7bd2d7532fff
mirekmts <mirekmts@gmail.com>
2018-11-11 17:25:00 +0100

    Setup eslint and enzyme

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "no-undef": 0,
+    "react/jsx-filename": 0,
+    "import/no-named-as": 0,
+    "import/no-extraneous-dependencies": 0,
+    "import/named": 0,
+    "react/jsx-filename-extension": 0,
+    "max-len": [1, { "code": 120}],
+    "react/destructuring-assignment": 0,
+    "react/prefer-stateless-function": 0,
+    "react/jsx-one-expression-per-line": 0,
+  }
+};
